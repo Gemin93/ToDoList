520fbf52b07e7cbaedb280f6f538b04570e1a5bd
Роман Медведцкий <roman.medvedtckiy@gmail.com>
2023-02-07 19:09:01 +0300

    test route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,29 @@ import React, { FC } from 'react';
 import {Title} from "../Title/Title";
 import {AddTask} from "../AddTask/AddTask";
 import {TodoList} from "../TodoList/TodoList";
+import {Route, Switch} from "react-router-dom";
+import {SignIn} from "../SignIn/SignIn";
+import {SignUp} from "../SignUp/SignUp";
 
 export const App: FC = () => {
   return (
     <>
-      <div>
-        <Title/>
-      </div>
-      <div>
-        <AddTask/>
-      </div>
-      <TodoList/>
+      <Title/>
+      <Switch>
+        <Route path="/account">
+          <div>
+            <AddTask/>
+            <TodoList/>
+          </div>
+        </Route>
+        <Route path="/">
+          <SignIn/>
+        </Route>
+        <Route path="/signup">
+          <SignUp/>
+        </Route>
+      </Switch>
+
     </>
   );
 };
